06524cc95827a33f522cec4b34235636c7ddc08b
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-13 22:57:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,6 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.upcase
-    puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
 end
